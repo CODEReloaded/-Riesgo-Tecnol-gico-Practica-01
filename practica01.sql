81b39510ef2c2fe2e47dd7eaf4a51d830ae7ce7f
JonAbrego <abrego.fc@gmail.com>
2016-03-01 16:19:14 -0600

    Modifique la relacion de la base de datos

--- practica01.sql
+++ practica01.sql
@@ -3,7 +3,6 @@ id SERIAL PRIMARY KEY,
 nombres TEXT NOT NULL,
 apellido_p TEXT NOT NULL, 
 apellido_m TEXT NOT NULL);
-
 CREATE TABLE venta(
 id SERIAL PRIMARY KEY,
 total REAL NOT NULL,
@@ -11,9 +10,9 @@ neto REAL NOT NULL,
 impuestos REAL NOT NULL
 );
 CREATE TABLE captura(
+id_capturista SERIAL NOT NULL, 
+id_venta SERIAL NOT NULL, 
 fecha date NOT NULL,
-id_capturista INT NOT NULL, 
-id_venta INT NOT NULL, 
 FOREIGN KEY (id_capturista) REFERENCES capturista(id),
 FOREIGN KEY (id_venta) REFERENCES venta(id)
 );
